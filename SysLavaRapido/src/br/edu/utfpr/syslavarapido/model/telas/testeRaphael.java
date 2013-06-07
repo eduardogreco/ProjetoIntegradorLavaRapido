227bd8c80ccd05f3ae721cabe7e310fad9ad5ee1
RAFAEL <RAFAEL@10.10.28.236>
2013-06-07 20:37:25 -0300

    testes raphael

---
+++ SysLavaRapido/src/br/edu/utfpr/syslavarapido/model/telas/testeRaphael.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.utfpr.syslavarapido.model.telas;
+
+/**
+ *
+ * @author RAFAEL
+ */
+public class testeRaphael {
+    
+}
